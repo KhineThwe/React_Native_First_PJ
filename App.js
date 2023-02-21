82ea57465d848e893ee600d01b84472a40b36240
Khine <khinezar70122107@gmail.com>
2023-02-20 12:12:37 -1200

    todoapp update delete

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React,{useState} from 'react';
-import {View,Text,TextInput,StyleSheet,Button,ScrollView} from 'react-native';
+import {View,Text,TextInput,StyleSheet,Button,ScrollView,FlatList,TouchableOpacity} from 'react-native';
 
 const App = () =>{
   const [todos,setTodos] = useState([]);
@@ -11,8 +11,32 @@ const App = () =>{
       // newTodos.push(text);
       // setTodos(newTodos);
       //or
-      setTodos([...todos,text]);
+      setTodos([...todos,
+        {
+          value:text,
+          id:new Date().toISOString()
+        }
+      ]);
     }
+  };
+  const deleteTodo = (id)=>{
+    let newTodos = todos.filter((todo)=>todo.id!==id);
+    setTodos(newTodos)
+  };
+  const updateTodo = (id)=>{
+    // let newTodos = todos.filter((todo)=>todo.id!==id);
+    // let toupdate = todos.filter((todo)=>todo.id==id);
+    // toupdate[0].value = 'value';
+    // setTodos([...toupdate,...newTodos]);
+    //or
+    let newTodos = []
+    for(let i=0;i<todos.length;i++){
+      if(todos[i].id == id){
+        todos[i].value = 'updated'
+      }
+      newTodos.push(todos[i])
+    }
+    setTodos(newTodos)
   };
   return (
     <View style={styles.root}>
@@ -21,13 +45,26 @@ const App = () =>{
       <TextInput style={styles.input} value={text} onChangeText={(value)=>setText(value)}/>
       <Button title="Add" onPress={handlePress}/>
     </View>
-    <ScrollView>
+    {/* <ScrollView>
     {
     todos.map(
       (todo,i)=>(<Text key={i}>{todo}</Text>)
     )
     }
-    </ScrollView>
+    </ScrollView> */}
+    <FlatList data={todos} renderItem={({item})=>(
+    <View style={styles.viewStyle}>
+      <Text style={styles.text}>{item.value}</Text>
+      <TouchableOpacity onPress={()=>deleteTodo(item.id)}>
+        <Text>Delete</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={()=>updateTodo(item.id)}>
+        <Text>Update</Text>
+    </TouchableOpacity>
+    </View>
+    )}
+    keyExtractor={(item)=>item.id}
+    />
   </View>
   );
 };
@@ -52,6 +89,15 @@ const styles = StyleSheet.create({
   },
   inputWrapper:{
     flexDirection : "row",
+  },
+  text:{
+    fontSize:15,
+    paddingVertical: 10,
+  },
+  viewStyle:{
+    flexDirection:'row',
+    justifyContent:'space-between',
+    alignItems:"center",
   }
 })
 export default App;
